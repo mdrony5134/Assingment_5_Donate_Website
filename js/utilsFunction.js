5928dc4380d65052d20d7496a9a4c26939f46952
mdrony5134 <rony830388@gmail.com>
2024-09-23 01:15:03 +0600

    donation functionality added

---
+++ js/utilsFunction.js
@@ -0,0 +1,7 @@
+function getInputValueById(id){
+    return parseFloat(document.getElementById(id).value)
+}
+
+function getvalueByInnerText(id){
+    return parseFloat(document.getElementById(id).innerText)
+}
