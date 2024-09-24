5dbc2e2e57412924670cdf2177671cf397ca9477
mdrony5134 <rony830388@gmail.com>
2024-09-24 12:47:38 +0600

    backeground blur added when scroll functionality

--- js/feature.js
+++ js/feature.js
@@ -1,4 +1,3 @@
-
 // featur section in button tab
 function tabButton(historyID, donationID, showSectionId, hiddenSectionId) {
   document.getElementById(historyID).className =
@@ -10,7 +9,6 @@ function tabButton(historyID, donationID, showSectionId, hiddenSectionId) {
   document.getElementById(hiddenSectionId).classList.add("hidden");
 }
 
-
 document.getElementById("btn_history").addEventListener("click", function () {
   tabButton("btn_history", "btn_donation", "show_history", "show_donation");
 });
@@ -18,23 +16,34 @@ document.getElementById("btn_donation").addEventListener("click", function () {
   tabButton("btn_donation", "btn_history", "show_donation", "show_history");
 });
 
-
 // blog button functionality
-document.getElementById("btn_blog").addEventListener("click",function(){
-  window.location.href = "/blog.html"
-})
+document.getElementById("btn_blog").addEventListener("click", function () {
+  window.location.href = "/blog.html";
+});
 
 // scroll behavior  sticky btn_group section
 
-window.addEventListener("scroll", function(){
-  const btnGroup = document.getElementById("btn_group")
+window.addEventListener("scroll", function () {
+  const btnGroup = document.getElementById("btn_group");
   const scrollPosition = window.scrollY;
 
-  if(scrollPosition > 100){
-    btnGroup.classList.remove("bg-[#F9F7F3]")
-    btnGroup.classList.add("bg-gradient-to-r", "from-[#F9F7F3]", "to-[#F9F7F3]")
-  }else{
-    btnGroup.classList.add("bg-white")
-    btnGroup.classList.remove("bg-gradient-to-r", "from-[#F9F7F3]", "to-[#F9F7F3]")
+  if (scrollPosition > 100) {
+    btnGroup.classList.remove("bg-[#F9F7F3]");
+    btnGroup.classList.add(
+      "bg-gradient-to-r",
+      "from-[#F9F7F3] / 70",
+      "to-[#F9F7F3] / 70",
+      "backdrop-blur-md",
+      "backdrop-filter"
+    );
+  } else {
+    btnGroup.classList.add("bg-white");
+    btnGroup.classList.remove(
+      "bg-gradient-to-r",
+      "from-[#F9F7F3] / 80",
+      "to-[#F9F7F3] / 80",
+      "backdrop-blur-md",
+      "backdrop-filter"
+    );
   }
-})
+});
