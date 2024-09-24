bc5402a69559c41105a7ea82115617fe0c78e230
mdrony5134 <rony830388@gmail.com>
2024-09-24 21:24:09 +0600

    check all code and some modify code and updated

--- js/utilsFunction.js
+++ js/utilsFunction.js
@@ -21,33 +21,41 @@ function handleDonationCalculation(
   const amount = getvalueByInnerText("amount");
   const currentAmount = getvalueByInnerText(currentDonationId);
   const donateHeading = getInnerText(headingId);
-  const succeModal = document.getElementById(succeModalId)
+  const succeModal = document.getElementById(succeModalId);
 
   // handle validation
-  if (isNaN(donateValue) || donateValue <= 0 || amount < donateValue) {
+  if (isNaN(donateValue) || donateValue <= 0) {
     return alert("Invalid donation amount");
+  }
+
+  if (amount < donateValue) {
+    return alert("Insufficent Blance");
   }
 
   const newDonateAmount = currentAmount + donateValue;
   const newAmount = amount - donateValue;
-  document.getElementById(
-    showAmountId
-  ).innerHTML = `<h3 id="${currentDonationId}" class="font-medium text-lg"> ${newDonateAmount} BDT </h3>`;
-  document.getElementById(
-    "show_amount"
-  ).innerHTML = `<h3 id="amount" class="text-lg font-medium">${newAmount} BDT</h3>`;
+
+  document.getElementById(showAmountId).innerHTML = `
+  <h3 id="${currentDonationId}" class="font-medium text-lg"> ${newDonateAmount} BDT </h3>`;
+
+  document.getElementById("show_amount").innerHTML = `
+  <h3 id="amount" class="text-lg font-medium">${newAmount} BDT</h3>
+  `;
+
   document.getElementById(donatioAmountID).value = "";
 
   const currentDate = new Date().toLocaleString();
-  succeModal.showModal()
+  succeModal.showModal();
 
   // history functionality
   const newHistory = document.createElement("div");
   newHistory.innerHTML = `
    <div id="updated_history" class="bg-white mt-4 shadow-md rounded-lg p-8 border border-[#1111111A]">
-              <h1 class="text-xl font-bold pb-4">${newDonateAmount} Taka ${donateHeading}</h1>
-  <p>Date : ${currentDate}</p>
-            </div>
+      <h1 class="text-xl font-bold pb-4">
+      ${newDonateAmount} Taka ${donateHeading}
+      </h1>
+      <p>Date : ${currentDate}</p>
+  </div>
   `;
   document.getElementById("history_container").appendChild(newHistory);
 }
